ce38202e8b8f1c5f93ade3c5c1b0ca4e9fddd544
mortzavoy57 <mortzavoy57@gmail.com>
2026-08-26 22:43:12 +0300

    Rename ExampleJavaMod to MindTech

--- src/example/MindTech.java
+++ src/example/MindTech.java
@@ -6,9 +6,9 @@ import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class MindTech extends Mod{
 
-    public ExampleJavaMod(){
+    public MindTech(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
