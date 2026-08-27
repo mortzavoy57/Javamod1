da5ea21c372593823c4bb5e8831e51aeee116a0c
mortzavoy57 <mortzavoy57@gmail.com>
2026-08-27 17:52:36 +0300

    Add files via upload

---
+++ src/mindtech/MindTech.java
@@ -0,0 +1,22 @@
+package mindtech;
+
+import arc.*;
+import arc.util.*;
+import mindtech.content.MTItems;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class MindTech extends Mod{
+
+    public MindTech(){
+        Log.info("Loaded ExampleJavaMod constructor.");
+    }
+
+    public void loadContent(){
+        MTItems.load();
+
+        Log.info("MindTech Loaded Complete.");
+    }
+
+}
